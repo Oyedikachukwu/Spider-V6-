e049f927eaf6baa357599d8f8feb973c8902fd52
Oyedikachukwu <obiohadestiny8@gmail.com>
2024-08-24 17:03:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,17 @@
 
 
 require("./all/module")
-global.owner = "254742491666" 
+global.owner = "2349138466675" 
 global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
 global.autoJoin = false 
 global.antilink = false 
 global.versisc = '6.0' 
-global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
-global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
+global.codeInvite = "https://chat.whatsapp.com/KZffTgs8e9c7wkELTv0eyb"
+global.isLink = 'https://chat.whatsapp.com/KZffTgs8e9c7wkELTv0eyb'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.packname = "DESTINYOFFICIAL" 
 global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
 global.jumlah = "5" 
 global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
